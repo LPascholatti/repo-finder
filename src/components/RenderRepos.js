9968cdac26323225e93a460c409a684b9ac7057b
LPascholatti <lucaspascholatti@gmail.com>
2019-12-05 14:18:28 +0100

    Display results only if total repos different than undefined

--- src/components/RenderRepos.js
+++ src/components/RenderRepos.js
@@ -1,16 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class RenderRepos extends Component {
+  renderRepos(items) {
+    console.log("items", items);
+    const { name, description, url, language } = items;
+
+    return (
+      <div className="renderRepos">
+        <li key={name}>
+          <h4>{name}</h4>
+          <h5>{description}</h5>
+          <h5>
+            <a href={url}>{url}</a>
+          </h5>
+          <h5>{language}</h5>
+        </li>
+      </div>
+    );
+  }
 
   render() {
-
     const { repositories } = this.props;
 
     return (
       <div className="repositories-result">
         <h2>Repositories</h2>
-        <h3>{`You found ${repositories.total_count} repositories`}</h3>
+        <h3>{repositories.total_count !== undefined && `You found ${repositories.total_count} repositories`}</h3>
+        <br />
       </div>
-    )
+    );
   }
 }

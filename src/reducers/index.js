26803d292fa1346c4d76aed20de185ddcf5af557
LPascholatti <lucaspascholatti@gmail.com>
2019-12-06 15:54:51 +0100

    Stepping back from previous details implementation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import repositories from '../reducers/repositories';
-import repository from '../reducers/repository';
 import name from './name'
 
 export default combineReducers({
   repositories,
-  repository,
   name
 })

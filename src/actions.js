9f4d529403c3a8c39b87ee3b76f7504b63a15aa0
LPascholatti <lucaspascholatti@gmail.com>
2019-12-09 09:27:43 +0100

    Changed lifetime cycles in RenderDetails and RenderReadme to avoid excessive API requests, plus comment out lines in onChange method, avoiding excessive requests

--- src/actions.js
+++ src/actions.js
@@ -1,72 +1,72 @@
-import request from 'superagent';
+import request from "superagent";
 
-const baseUrl = 'https://api.github.com'
+const baseUrl = "https://api.github.com";
 
-export const REPOSITORIES_FETCH = 'REPOSITORIES_FETCH'
-export const REPOSITORY_FETCH = 'REPOSITORY_FETCH'
-export const SEARCHED_NAME = 'SEARCHED_NAME'
-export const REPOSITORIES_ITEMS = 'REPOSITORIES_ITEMS'
-export const DETAIL_ID = "DETAIL_ID"
-export const README_FETCH = "README_FETCH"
+export const REPOSITORIES_FETCH = "REPOSITORIES_FETCH";
+export const REPOSITORY_FETCH = "REPOSITORY_FETCH";
+export const SEARCHED_NAME = "SEARCHED_NAME";
+export const REPOSITORIES_ITEMS = "REPOSITORIES_ITEMS";
+export const DETAIL_ID = "DETAIL_ID";
+export const README_FETCH = "README_FETCH";
 
 const repositoriesFetch = repositories => ({
   type: REPOSITORIES_FETCH,
   payload: repositories
-})
+});
 
 const repositoryFetch = repository => ({
   type: REPOSITORY_FETCH,
   payload: repository
-})
+});
 
 const searchedName = name => ({
   type: SEARCHED_NAME,
   payload: name
-})
+});
 
 const detailedId = id => ({
   type: DETAIL_ID,
   payload: id
-})
+});
 
 const requestReadme = readme => ({
   type: README_FETCH,
   payload: readme
-})
+});
 
-export const loadRepositories = (name) => (dispatch, getState) => {
-  const state = getState()
-  const { repositories } = state
+export const loadRepositories = name => (dispatch, getState) => {
+  const state = getState();
+  const { repositories } = state;
 
   if (!repositories.length) {
     request(`${baseUrl}/search/repositories?q=${name}`)
-    .then(response => {
-      const fetchAction = repositoriesFetch(response.body)
-      const searchAction = searchedName(name)
-      dispatch(fetchAction)
-      dispatch(searchAction)
-    })
-    .catch(console.error)
+      .then(response => {
+        const fetchAction = repositoriesFetch(response.body);
+        const searchAction = searchedName(name);
+        dispatch(fetchAction);
+        dispatch(searchAction);
+      })
+      .catch(console.error);
   }
-}
+};
 
-export const loadRepository = (id) => (dispatch) => {
+export const loadRepository = id => dispatch => {
   request(`${baseUrl}/repositories/${id}`)
-  .then(response => {
-    const actionId = detailedId(id)
-    const actionResponse = repositoryFetch(response.body)
-    dispatch(actionId)
-    dispatch(actionResponse)
-  })
-  .catch(console.error)
-}
+    .then(response => {
+      const actionId = detailedId(id);
+      const actionResponse = repositoryFetch(response.body);
+      dispatch(actionId);
+      dispatch(actionResponse);
+    })
+    .catch(console.error);
+};
 
-export const loadReadme = (owner, name) => (dispatch) => {
+export const loadReadme = (owner, name) => dispatch => {
   request(`${baseUrl}/repos/${owner}/${name}/readme`)
-  .set('Accept', 'application/vnd.github.v3.html')
-  .then(response => {
-    const readmeAction = requestReadme(response)
-    dispatch(readmeAction)
-  })
-  .catch(console.error)
-}
+    .set("Accept", "application/vnd.github.v3.html")
+    .then(response => {
+      const readmeAction = requestReadme(response);
+      dispatch(readmeAction);
+    })
+    .catch(console.error);
+};
